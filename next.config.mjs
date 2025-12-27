1eb70af4a1c116fe675a4dc31e1cca3640142802
pankaj-yadav73 <pankajofficial080@gmail.com>
2025-12-27 21:16:59 +0530

    feat: add generated WASM loaders for Prisma query engine

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true
-    },
     experimental: {
         serverComponentsHmrCache: false, // defaults to true\
     },
